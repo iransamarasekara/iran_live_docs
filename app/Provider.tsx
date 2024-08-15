06ad6ae139067db6361207d2e71f144f7dfd2d58
iransamarasekara <iransamarasekara@gmail.com>
2024-08-15 16:33:55 +0530

    first commit

---
+++ app/Provider.tsx
@@ -0,0 +1,34 @@
+'use client';
+import Loader from '@/components/Loader';
+import { getClerkUser, getDocumentUsers } from '@/lib/actions/user.action';
+import { useUser } from '@clerk/nextjs';
+import { ClientSideSuspense, LiveblocksProvider } from '@liveblocks/react/suspense';
+import { ReactNode, use } from 'react';
+
+const provider = ({ children }: {children: ReactNode}) => {
+  const {user: clerkUser} = useUser();
+  return (
+    <LiveblocksProvider 
+      authEndpoint={"/api/liveblocks-auth"}
+      resolveUsers={async ({userIds}) => {
+        const users = await getClerkUser({userIds});
+        return users;
+      }}
+      resolveMentionSuggestions={async ({ text, roomId }) => {
+        const roomUsers = await getDocumentUsers({
+          roomId,
+          currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+          text,
+        })
+
+        return roomUsers;
+      }}
+    >
+        <ClientSideSuspense fallback={<Loader />}>
+            {children}
+        </ClientSideSuspense>
+    </LiveblocksProvider>
+  )
+}
+
+export default provider
